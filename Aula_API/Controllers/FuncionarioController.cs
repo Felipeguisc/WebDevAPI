1a69802afca3222e0cd5ae9a779cc1c41bc455ee
Felipe Guimaraes <felipeguisc@gmail.com>
2023-09-05 19:18:50 -0300

    Initial Commit

---
+++ Aula_API/Controllers/FuncionarioController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FuncionarioController : ControllerBase
+{
+    private readonly ILogger<FuncionarioController> _logger;
+
+    public FuncionarioController(ILogger<FuncionarioController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetFuncionario")]
+    public IActionResult Get()
+    {
+        return Ok("get TODOS executado");
+    }
+
+    [HttpGet("{id:int}", Name = "GetWeather")]
+    public IActionResult Get(int id)
+    {
+        return Ok("get UM executado");
+    }
+
+    [HttpPost(Name = "PostFuncionario")]
+    public IActionResult Post()
+    {
+        return Ok("post executado");
+    }
+
+    [HttpPut("{id:int}", Name = "PutFuncionario")]
+    public IActionResult Put(int id)
+    {
+        return StatusCode(201, "put executado");
+    }
+
+    [HttpDelete("{id:int}", Name = "DeleteFuncionario")]
+    public IActionResult Delete(int id)
+    {
+        return StatusCode(201, "delete executado");
+    }
+}
+
