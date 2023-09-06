8e56110401492f1086705a4d264455088034c653
Felipe Guimaraes <felipeguisc@gmail.com>
2023-09-06 00:37:43 -0300

    Added CRUD actions to ClienteController and ProdutoController

--- Aula_API/Controllers/ClienteController.cs
+++ Aula_API/Controllers/ClienteController.cs
@@ -16,12 +16,96 @@ public class ClienteController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetCliente")]
+    [HttpGet(Name = "GetClientes")]
     public IActionResult Get()
     {
-        var clients = _dbContext.Clientes.ToList();
+        try
+        {
+            var clientes = _dbContext.Clientes.ToList();
 
-        return Ok(clients);
+            return Ok(clientes);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while Getting the GetClientes. Error: " + ex.Message);
+        }
+    }
+
+    [HttpGet("{id:int}", Name = "GetCliente")]
+    public IActionResult Get(int id)
+    {
+        try
+        {
+            var cliente = _dbContext.Clientes.FirstOrDefault(e => e.Id == id);
+
+            if (cliente != null)
+            {
+                return Ok(cliente);
+            }
+            else
+            {
+                return NotFound("Nenhum Cliente encontrado");
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while Getting the Cliente. Error: " + ex.Message);
+        }
+    }
+
+    [HttpPost(Name = "PostCliente")]
+    public IActionResult Post(Cliente cliente)
+    {
+        try
+        {
+            var result = _dbContext.Clientes.Add(cliente);
+            _dbContext.SaveChanges();
+            return Ok("post executado");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while adding the Cliente. Error: " + ex.Message);
+        }
+    }
+
+    [HttpPut(Name = "PutCliente")]
+    public IActionResult Put(Cliente cliente)
+    {
+        try
+        {
+            var result = _dbContext.Clientes.Update(cliente);
+            _dbContext.SaveChanges();
+            return StatusCode(201, "put executado");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while updating the Cliente. Error: " + ex.Message);
+        }
+    }
+
+    [HttpDelete("{id:int}", Name = "DeleteCliente")]
+    public IActionResult Delete(int id)
+    {
+        try
+        {
+            var cliente = _dbContext.Clientes.FirstOrDefault(e => e.Id == id);
+
+            if (cliente != null)
+            {
+                var result = _dbContext.Clientes.Remove(cliente);
+                _dbContext.SaveChanges();
+
+                return StatusCode(201, $"Entity with ID {cliente.Id} has been deleted.");
+            }
+            else
+            {
+                return StatusCode(404, "Entity not found, so it cannot be deleted.");
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while deleting the Cliente. Error: " + ex.Message);
+        }
     }
 }
 
--- Aula_API/Controllers/ProdutoController.cs
+++ Aula_API/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Aula_API.Controllers;
 
@@ -6,17 +7,105 @@ namespace Aula_API.Controllers;
 [Route("[controller]")]
 public class ProdutoController : ControllerBase
 {
+    private readonly MyDbContext _dbContext;
     private readonly ILogger<ProdutoController> _logger;
 
-    public ProdutoController(ILogger<ProdutoController> logger)
+    public ProdutoController(MyDbContext dbContext, ILogger<ProdutoController> logger)
     {
+        _dbContext = dbContext;
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetProduto")]
+    [HttpGet(Name = "GetProdutos")]
     public IActionResult Get()
     {
-        return Ok("get TODOS executado");
+        try
+        {
+            var produtos = _dbContext.Produtos.ToList();
+
+            return Ok(produtos);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while Getting the GetProdutos. Error: " + ex.Message);
+        }
+    }
+
+    [HttpGet("{id:int}", Name = "GetProduto")]
+    public IActionResult Get(int id)
+    {
+        try
+        {
+            var produto = _dbContext.Produtos.FirstOrDefault(e => e.Id == id);
+
+            if (produto != null)
+            {
+                return Ok(produto);
+            }
+            else
+            {
+                return NotFound("Nenhum Produto encontrado");
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while Getting the Produto. Error: " + ex.Message);
+        }
+    }
+
+    [HttpPost(Name = "PostProduto")]
+    public IActionResult Post(Produto produto)
+    {
+        try
+        {
+            var result = _dbContext.Produtos.Add(produto);
+            _dbContext.SaveChanges();
+            return Ok("post executado");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while adding the Produto. Error: " + ex.Message);
+        }
+    }
+
+    [HttpPut(Name = "PutProduto")]
+    public IActionResult Put(Produto produto)
+    {
+        try
+        {
+            var result = _dbContext.Produtos.Update(produto);
+            _dbContext.SaveChanges();
+            return StatusCode(201, "put executado");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while updating the Produto. Error: " + ex.Message);
+        }
+    }
+
+    [HttpDelete("{id:int}", Name = "DeleteProduto")]
+    public IActionResult Delete(int id)
+    {
+        try
+        {
+            var produto = _dbContext.Produtos.FirstOrDefault(e => e.Id == id);
+
+            if (produto != null)
+            {
+                var result = _dbContext.Produtos.Remove(produto);
+                _dbContext.SaveChanges();
+
+                return StatusCode(201, $"Entity with ID {produto.Id} has been deleted.");
+            }
+            else
+            {
+                return StatusCode(404, "Entity not found, so it cannot be deleted.");
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while deleting the Produto. Error: " + ex.Message);
+        }
     }
 }
 
